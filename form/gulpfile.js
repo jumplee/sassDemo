0b221c5917ee26112a5728623ca9321d45c786df
jumplee <liyuntao09@gmail.com>
2017-02-25 11:58:32 +0800

    精灵图和prefix

--- form/gulpfile.js
+++ form/gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp');
-var px2rem = require('gulp-px3rem');
+// var px2rem = require('gulp-px3rem');
 var autoprefixer = require('gulp-autoprefixer');
 gulp.task('px2rem', function () {
   gulp.src('./form.css')
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: [
+        'ios>7',
+        'android >4'
+      ],
       cascade: false
     }))
-    .pipe(px2rem({
-      baseDpr: 2, // base device pixel ratio (default: 2)
-      threeVersion: false, // whether to generate @1x, @2x and @3x version (default: false)
-      remVersion: true, // whether to generate rem version (default: true)
-      remUnit: 20, // rem unit value (default: 75)
-      remPrecision: 2 // rem precision (default: 6)
-    }))
+    // .pipe(px2rem({
+    //   baseDpr: 2, // base device pixel ratio (default: 2)
+    //   threeVersion: false, // whether to generate @1x, @2x and @3x version (default: false)
+    //   remVersion: true, // whether to generate rem version (default: true)
+    //   remUnit: 20, // rem unit value (default: 75)
+    //   remPrecision: 2 // rem precision (default: 6)
+    // }))
     .pipe(gulp.dest('./dest'))
 });
 gulp.task('default', ['px2rem'])
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,60 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var sprites = require('postcss-sprites');
+var post = require('postcss')
+var opts = {
+  stylesheetPath: './dist',
+  spritePath: './dist/images/'
+};
 gulp.task('prefix', function () {
-    return gulp.src('./form/form.css')
+  return gulp.src('./form/form.css')
     .pipe(postcss([
-        autoprefixer()
+      autoprefixer({
+        browsers: [
+          'ios>7',
+          'android >4'
+        ],
+        cascade: false
+      })
     ]))
+    .pipe(gulp.dest('./form/dist'))
 })
-var spritesmith = require('gulp.spritesmith');
 
+var updateRule = require('postcss-sprites/lib/core').updateRule;
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('sprite/images/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: 'sprite.css'
-  }));
-  return spriteData.pipe(gulp.dest('sprite/dist'));
-});
+  return gulp.src('./sprite/css/icon.css')
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          'ios>7',
+          'android >4'
+        ],
+        cascade: false
+      }),
+      sprites({
+        retina: true,
+        padding: 3,
+        stylesheetPath: './sprite/dist/css',
+        spritePath: './sprite/dist/images/',
+        hooks: {
+          onUpdateRule: function (rule, token, image) {
+            // Use built-in logic for background-image & background-position
+            updateRule(rule, token, image);
+       
+            ['width', 'height'].forEach(function (prop) {
+              rule.insertAfter(rule.last, post.decl({
+                prop: prop,
+                value: image.coords[prop] / image.ratio + 'px'
+              }));
+            });
+          }
+        }
+      })
+    ]))
+    .pipe(gulp.dest('./sprite/dist/css'))
+})
+
+gulp.task('watch_sprite', function () {
+  gulp.watch('./sprite/css/icon.css', ['sprite'])
+})
